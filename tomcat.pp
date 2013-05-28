03a20a54b4f3a13fa6e5049f74737741c90aeefe
Empowers33 <empowers33@gmail.com>
2013-05-28 17:17:57 +0000

    changed dir creation, templates, ordering

--- tomcat.pp
+++ tomcat.pp
@@ -1,66 +1,67 @@
 Exec {
-  path      => "/bin:/sbin:/usr/bin:/usr/sbin",
-  logoutput => "on_failure"
+  path       => "/bin:/sbin:/usr/bin:/usr/sbin",
+  #logoutput => "on_failure"
+  logoutput  => true
 }
 
 exec{"Download JDK":
   cwd     => '/tmp',
   command => "wget http://uni-smr.ac.ru/archive/dev/java/SDKs/sun/j2se/7/jdk-7u21-linux-i586.tar.gz",
-  creates => '/tmp/jdk-7u21-linux-x64.tar.gz',
+  creates => '/tmp/jdk-7u21-linux-i586.tar.gz',
+}
 
-  before => Exec["Extract JDK tar"]
+file{ '/root/java':
+  ensure => "directory"
 }
 
 exec{"Extract JDK tar":
-  command => "mkdir -p /root/java && cd root/java && tar zxvf /tmp/jdk-7u21-linux-i586",
-  creates => '/root/java',
-
-  before => File['/etc/profile.d/java.sh']
+  # check this
+  cwd     => '/root/java',
+  command => "tar zxvf /tmp/jdk-7u21-linux-i586.tar.gz",
+  creates => '/root/java/jdk1.7.0_21',
 }
 
 # Save env vars for JDK
 file{'/etc/profile.d/java.sh':
     ensure => present,
     
-    content => 'export JAVA_HOME=/root/java/jdk1.7.0_21
-                export PATH=$JAVA_HOME/bin:$PATH',
+    content => template("tomcat/JdkEnviromentVars.erb"),
 
     owner  => 'root',
     group  => 'root',
-    mode   => '644',
-
-    before => Exec["Download Tomcat"]
+    mode   => '644'
 }
 
 exec{"Download Tomcat":
   cwd     => '/tmp',
   command => "wget http://mirrors.besplatnyeprogrammy.ru/apache/tomcat/tomcat-6/v6.0.37/bin/apache-tomcat-6.0.37.tar.gz",
-  creates => '/tmp/apache-tomcat-6.0.37.tar.gz',
+  creates => '/tmp/apache-tomcat-6.0.37.tar.gz'
+}
 
-  before => Exec["Extract Tomcat tar"]
+file{'/root/tomcat6':
+  ensure => "directory"
 }
 
 exec{ "Extract Tomcat tar":
-  command => "mkdir -p /root/tomcat6 && cd /root/tomcat6 && tar zxvf /tmp/apache-tomcat-6.0.37.tar.gz",
-  creates => '/root/tomcat6',
-  
-  before => File['/etc/profile.d/catalina.sh']
+  # check this
+  cwd     => '/root/tomcat6',
+  command => "tar zxvf /tmp/apache-tomcat-6.0.37.tar.gz",
+  creates => '/root/tomcat6/bin'
 } 
 
 # Save env vars for tomcat
 file { '/etc/profile.d/catalina.sh':
   ensure  => present,
-  content => 'export CATALINA_BASE=/root/tomcat6/apache-tomcat-6.0.37
-              export CATALINA_HOME=$CATALINA_BASE
-              export CATALINA_TMPDIR=$CATALINA_BASE/temp',
+  content => template("tomcat/TomcatEnviromentVars.erb"),
+
   
   owner => 'root',
   group => 'root',
-  mode  => '644',
-
-  before => Exec["Start Tomcat"]
+  mode  => '644'
 }
 
 exec{ "Start Tomcat":
   command => "/root/tomcat6/apache-tomcat-6.0.37/bin/startup.sh"
 }
+
+Exec["Download JDK"] -> File['/root/java'] -> Exec["Extract JDK tar"] -> File['/etc/profile.d/java.sh'] -> Exec["Download Tomcat"] -> File['/root/tomcat6'] -> Exec["Extract Tomcat tar"] -> File['/etc/profile.d/catalina.sh'] -> Exec["Start Tomcat"] 
